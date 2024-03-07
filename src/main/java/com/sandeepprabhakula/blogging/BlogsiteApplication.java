8d7bb07ef0b817b5f679b7a3bee60fb66c55ee86
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-03-07 22:59:40 +0530

    Added Cron job for keeping api awake

--- src/main/java/com/sandeepprabhakula/blogging/BlogsiteApplication.java
+++ src/main/java/com/sandeepprabhakula/blogging/BlogsiteApplication.java
@@ -2,8 +2,10 @@ package com.sandeepprabhakula.blogging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BlogsiteApplication {
 
 	public static void main(String[] args) {
