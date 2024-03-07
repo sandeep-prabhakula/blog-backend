b28868d0eb51a88590a7f1310fe2228000b3568b
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-03-07 22:59:40 +0530

    Added Cron job for keeping api awake

--- src/main/java/com/sandeepprabhakula/blogging/config/SecurityConfig.java
+++ src/main/java/com/sandeepprabhakula/blogging/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 
 @EnableWebSecurity
 @Configuration
@@ -65,5 +66,10 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
