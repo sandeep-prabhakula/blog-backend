8371f6a18e7216202e3a78e151b6fa207c073196
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-06-22 14:34:58 +0530

    Changed response payload for the login endPoint

--- src/main/java/com/sandeepprabhakula/blogging/service/EmailService.java
+++ src/main/java/com/sandeepprabhakula/blogging/service/EmailService.java
@@ -2,7 +2,7 @@ package com.sandeepprabhakula.blogging.service;
 
 import com.sandeepprabhakula.blogging.data.User;
 import com.sandeepprabhakula.blogging.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,38 +12,37 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
-    @Autowired
-    private JavaMailSender mailSender;
-
+    private final JavaMailSender mailSender;
     @Value("${spring.mail.username}")
     private String sender;
-
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public ResponseEntity<?> sendPasswordResetEmail(String email) {
         try {
-            User user = userRepository.findByEmail(email).get();
-            if(user==null){
-                return new ResponseEntity<>(new Exception("User Not Found"),HttpStatus.NO_CONTENT);
+            Optional<User> optionalUser = userRepository.findByEmail(email);
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                String url = "https://codeverse-chronicles.vercel.app/setup-password?id=" + user.getId();
+                SimpleMailMessage mail = new SimpleMailMessage();
+                mail.setFrom(sender);
+                mail.setTo(email);
+                mail.setText("Click the below link for setting up your password. \n " + url);
+                mail.setSubject("Confirmation for password reset");
+                mailSender.send(mail);
+                Map<String, Object> map = new HashMap<>();
+                map.put("status", 200);
+                map.put("message", "Password reset email sent.");
+                return new ResponseEntity<>(map, HttpStatus.OK);
             }
-            String url = "https://codeverse-chronicles.vercel.app/setup-password?id="+user.getId();
-            SimpleMailMessage mail = new SimpleMailMessage();
-            mail.setFrom(sender);
-            mail.setTo(email);
-            mail.setText("Click the below link for setting up your password. \n "+url);
-            mail.setSubject("Confirmation for password reset");
-            mailSender.send(mail);
-            Map<String,Object> map = new HashMap<>();
-            map.put("status",200);
-            map.put("message","Password reset email sent.");
-            return new ResponseEntity<>(map,HttpStatus.OK);
+            return new ResponseEntity<>(new Exception("User Not Found"), HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 }
