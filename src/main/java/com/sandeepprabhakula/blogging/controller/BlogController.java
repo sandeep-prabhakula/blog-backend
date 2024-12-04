9bbd6eeeced5a1cd3cffcb42e589a03bb22e7514
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-12-04 23:47:20 +0530

    security patch logging the ip addr

--- src/main/java/com/sandeepprabhakula/blogging/controller/BlogController.java
+++ src/main/java/com/sandeepprabhakula/blogging/controller/BlogController.java
@@ -2,7 +2,10 @@ package com.sandeepprabhakula.blogging.controller;
 
 import com.sandeepprabhakula.blogging.data.Blog;
 import com.sandeepprabhakula.blogging.service.BlogService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,24 +19,29 @@ import java.util.List;
 public class BlogController {
 
     private final BlogService blogService;
-
+    private final HttpServletRequest request;
+    private Logger log = LoggerFactory.getLogger(BlogController.class);
 
     @GetMapping("/get-all-blogs")
     public List<Blog> getAllBlogs(
             @RequestParam(name = "pageNumber", defaultValue = "0", required = false) int pageNumber,
             @RequestParam(name = "pageSize", defaultValue = "5",required = false)int pageSize
             ){
+
+        getIPAddress("get-all-blogs");
         return blogService.getAllBlogs(pageNumber,pageSize);
     }
 
     @GetMapping("/blog/{id}")
     public Blog getBlogById(@PathVariable("id")String id){
+        getIPAddress("blog");
         return blogService.findBlogById(id);
     }
 
     @PostMapping("/add-blog")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<String> addNewBlog(@RequestBody Blog blog){
+        getIPAddress("add-blog");
         String response = blogService.addNewBlog(blog);
         if(response.equals("401 Unauthorized"))return new ResponseEntity<>(HttpStatusCode.valueOf(401));
         return ResponseEntity.ok(response);
@@ -42,6 +50,7 @@ public class BlogController {
     @PutMapping("/update-blog")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<String> updateBlog(@RequestBody Blog blog){
+        getIPAddress("update-blog");
         String response = blogService.updateBlog(blog);
         if(response.equals("401 Unauthorized"))return new ResponseEntity<>(HttpStatusCode.valueOf(401));
         return ResponseEntity.ok(response);
@@ -50,6 +59,7 @@ public class BlogController {
     @DeleteMapping("/delete-blog/{blogId}")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<String> deleteBlog(@PathVariable("blogId")String blogId){
+        getIPAddress("delete-blog");
         String response = blogService.deleteBlog(blogId);
         if(response.equals("401 Unauthorized"))return new ResponseEntity<>(HttpStatusCode.valueOf(401));
         return ResponseEntity.ok(response);
@@ -57,7 +67,16 @@ public class BlogController {
 
     @GetMapping("/search-blogs/{prompt}")
     public List<Blog> search(@PathVariable("prompt")String prompt){
+        getIPAddress("search-blogs");
         return blogService.search(prompt);
+    }
+
+    private void getIPAddress(String endpoint){
+        try {
+            log.info("{} accessed from ip addr: {}", endpoint,request.getRemoteAddr());
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
     }
 
 }
